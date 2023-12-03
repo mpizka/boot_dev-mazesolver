00a708d549e1cbc8724dc3f1b5be2b2f0b33b8f9
Manfred Pizka <mpizka@icloud.com>
2023-12-03 21:17:52 +0100

    step 5: add Cell class
    This is again a bit different from the example, and not sure if its a
    good idea. Cell is assumed to be a square. Drawing logic relies on magic
    method allowing adding to points.

--- src/maze.py
+++ src/maze.py
@@ -8,26 +8,8 @@ GUI based on the Tkinter library.
 from dataclasses import dataclass
 from tkinter import Tk, BOTH, Canvas
 
-
-@dataclass
-class Point:
-    """A point in a classic canvas-coordinate system.
-
-    x=0, y=0 is the upper left corner
-    """
-
-    x: int = 0
-    y: int = 0
-
-
-@dataclass
-class Line:
-    """A line between two points in a classic canvas-coordinate system."""
-
-    p1: Point
-    p2: Point
-    fill_color: str
-    width: int = 2
+from typing import Self
+import time
 
 
 class Window:
@@ -73,12 +55,14 @@ class Window:
         self.__running = True
         while self.__running:
             self.redraw()
+            # rate limited so the CPU doesn't go crazy during tests
+            time.sleep(0.1)
 
     def close(self):
         """Handler for WM_DELETE_WINDOW protocol."""
         self.__running = False
 
-    def draw_line(self, line: Line):
+    def draw_line(self, line: "Line"):
         """Draw an instance of Line on the canvas.
 
         I changed this from the example, because it makes more sense to me,
@@ -95,9 +79,77 @@ class Window:
         )
 
 
+@dataclass
+class Point:
+    """A point in a classic canvas-coordinate system.
+
+    x=0, y=0 is the upper left corner
+    """
+
+    x: int
+    y: int
+
+    def __add__(self, other: tuple[int, int] | Self):
+        """Add a tuple (int,int) or another point to a point."""
+        if isinstance(other, tuple):
+            return Point(self.x + other[0], self.y + other[1])
+        if isinstance(other, Point):
+            return Point(self.x + other.x, self.y + other.y)
+        raise TypeError(
+            "unsupported operand type(s) for +: "
+            f"'{self.__class__.__name__}' and '{other.__class__.__name__}'"
+        )
+
+
+@dataclass
+class Line:
+    """A line between two points in a classic canvas-coordinate system."""
+
+    p1: Point
+    p2: Point
+    fill_color: str
+    width: int = 2
+
+
+@dataclass
+class Cell:
+    """A cell on the mazes grid."""
+
+    # window referenc
+    win: Window
+    # top left corner coordinates
+    top_left: Point
+    # walls
+    has_left_wall: bool = True
+    has_right_wall: bool = True
+    has_top_wall: bool = True
+    has_bottom_wall: bool = True
+    # edge length
+    edge_length: int = 20
+
+    def draw(self):
+        """Draw cell to the windows canvas."""
+        tl = self.top_left
+        w = self.edge_length
+        c = "black"
+
+        if self.has_left_wall:
+            self.win.draw_line(Line(tl, tl + (0, w), c))
+        if self.has_top_wall:
+            self.win.draw_line(Line(tl, tl + (w, 0), c))
+        if self.has_right_wall:
+            self.win.draw_line(Line(tl + (w, 0), tl + (w, w), c))
+        if self.has_bottom_wall:
+            self.win.draw_line(Line(tl + (0, w), tl + (w, w), c))
+
+
 # run the application
 if __name__ == "__main__":
     win = Window(800, 600)
-    win.draw_line(Line(Point(0, 0), Point(100, 100), "red"))
-    win.draw_line(Line(Point(100, 0), Point(0, 100), "red"))
+    win.draw_line(Line(Point(100, 100), Point(200, 200), "red"))
+    win.draw_line(Line(Point(200, 100), Point(100, 200), "red"))
+    Cell(win, Point(90, 90)).draw()
+    Cell(win, Point(190, 190), has_left_wall=False).draw()
+    Cell(win, Point(190, 90), has_right_wall=False, has_left_wall=False).draw()
+    Cell(win, Point(90, 190), has_top_wall=False, has_bottom_wall=False).draw()
     win.wait_for_close()
